dae5d2788f91c7f0644b1c2056f505978d47c93d
Geri Ceka <me@gericeka.com>
2022-06-23 11:45:51 +0200

    Second Commit

--- app.js
+++ app.js
@@ -5,71 +5,80 @@ const todoList = document.querySelector(".todo-list");
 
 //Add Todo Listener
 todoButton.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleted);
-todoList.addEventListener("dblclick", saveTodo);
+todoList.addEventListener("click", clickHandler);
 
 function addTodo(event) {
   event.preventDefault();
   if (todoButton.previousElementSibling.value.length === 0) {
-    todoList.remove();
     alert("Please fill the blanket input");
-  } 
+    return;
+  }
 
-  // TODO Div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
+  // // TODO Div
+  // const todoDiv = document.createElement("div");
+  // todoDiv.classList.add("todo");
 
-  //CREATE LI
-  const newTodo = document.createElement("li");
+  // //CREATE LI
+  // const newTodo = document.createElement("li");
+  // newTodo.innerText = todoInput.value;
+  // newTodo.classList.add("todo-item");
+  // newTodo.contentEditable = false;
+  // todoDiv.appendChild(newTodo);
+
+  // CREATE LI
+  const todoLi = document.createElement("li");
+  todoLi.classList.add("todo");
+
+  //Create Div
+  const newTodo = document.createElement("div");
   newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   newTodo.contentEditable = false;
-  todoDiv.appendChild(newTodo);
+  todoLi.appendChild(newTodo); 
 
   //Edit button
   const editBtn = document.createElement("button");
   editBtn.innerText = "Edit";
   editBtn.classList.add("edit-btn");
-  todoDiv.appendChild(editBtn);
+  todoLi.appendChild(editBtn);
+
+  //Save button
+  const saveBtn = document.createElement("button");
+  saveBtn.innerText = "Save";
+  saveBtn.classList.add("save-btn");
+  todoLi.appendChild(saveBtn);
 
   //Delete button
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "Delete";
   deleteBtn.classList.add("delete-btn");
-  todoDiv.appendChild(deleteBtn);
+  todoLi.appendChild(deleteBtn);
 
   //Append to list
-  todoList.appendChild(todoDiv);
+  todoList.appendChild(todoLi);
 
   //Clear Input
   todoInput.value = "";
 }
 
-function deleted(event) {
+function clickHandler(event) {
   const item = event.target;
   //Delete TODO
-  if (item.classList[0] === "delete-btn") {
+  if (item.classList.contains("delete-btn")) {
     const todo = item.parentElement;
     todo.remove();
   }
 
   //Edit TODO
-  if (item.classList[0] === "edit-btn") {
-    // const todo = item.previousElementSibling.innerHTML;
+  if (item.classList.contains("edit-btn")) {
     item.previousElementSibling.contentEditable = true;
     item.previousElementSibling.focus();
-    item.innerText = "Save";
-    item.classList.add("save-todo");
-    console.log(item);
-  }
-}
-
-function saveTodo(event) {
-  const item = event.target;
-
-  if (item.classList[1] === "save-todo") {
+    item.style.display = "none";
+    item.nextElementSibling.style.display = "block";
+  } else if (item.classList.contains("save-btn")) {
     item.previousElementSibling.contentEditable = false;
     item.previousElementSibling.blur();
-    item.innerText = "Edit";
+    item.style.display = "none";
+    item.previousElementSibling.style.display = "block";
   }
 }
